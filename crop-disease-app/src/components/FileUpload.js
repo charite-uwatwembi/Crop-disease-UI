b418d303197f1e1a7dfa85eecaa640f3cdea9b5a
charite-uwatwembi <c.uwatwembi@alustudent.com>
2024-10-27 20:01:52 +0200

    fileupload

--- crop-disease-app/src/components/FileUpload.js
+++ crop-disease-app/src/components/FileUpload.js
@@ -38,13 +38,13 @@ const UploadForm = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-green-100 p-6">
-      <h1 className="text-4xl font-bold text-green-700 mb-4">Upload a picture for prediction</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="file" onChange={handleFileChange} />
-        <button type="submit">Upload</button>
-      </form>
-      {message && <p>{message}</p>}
-    </div>
+    <h1 className="text-4xl font-bold text-green-700 mb-4">Upload a picture for prediction</h1>
+    <form onSubmit={handleSubmit} className="w-full max-w-md p-6 bg-white rounded-lg shadow-md">
+      <input type="file" onChange={handleFileChange} className="w-full p-2 mb-4 border border-gray-300 rounded-lg" />
+      <button type="submit" className="w-full bg-green-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700">Upload</button>
+    </form>
+    {message && <p className="text-lg text-gray-600 mt-4">{message}</p>}
+  </div>
   );
 };
 
